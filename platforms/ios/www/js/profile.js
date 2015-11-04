8ecc46896699abd53953ca30bfb4d94241a96f93
Lance Laughlin <llaughlin@sapient.com>
2015-11-04 13:55:17 -0500

    Initial commit

---
+++ platforms/ios/www/js/profile.js
@@ -0,0 +1,10 @@
+var $scope = $('.profile');
+var $profileBlock = $scope.find('.profile-hero .status');
+
+$profileBlock.on("touchstart",function(e){
+	var text = $(e.currentTarget).text();
+	$(e.currentTarget).text(
+		text == "Status: Inactive" ? "Status: Active" : "Status: Inactive"
+	);
+	$(e.currentTarget).closest(".profile-hero").toggleClass("active");
+});
