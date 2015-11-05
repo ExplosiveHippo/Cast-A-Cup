8e7d92fb43f363f94005919b0de1330c67f6515f
Lance Laughlin <llaughlin@sapient.com>
2015-11-05 13:50:06 -0500

    Social page done

---
+++ www/js/social.js
@@ -0,0 +1,18 @@
+var $scope = $('.social');
+var $icons = $scope.find(".social-toggle");
+
+$icons.on("click",function(e){
+	var img = $(e.currentTarget).find("img");
+	var src = $(img).attr("src");
+	if(src.indexOf("-on") > 0 ){
+		src = src.replace("-on","");
+		$(img).css("left",0);
+		$(img).siblings("p").text("off");
+	}else{
+		src = src.replace(".png","-on.png");
+		$(img).css("left",270);
+		$(img).siblings("p").text("on");
+	}
+	$(e.currentTarget).find("img").attr("src",src);
+	//$(e.currentTarget).find("img").src();
+});
